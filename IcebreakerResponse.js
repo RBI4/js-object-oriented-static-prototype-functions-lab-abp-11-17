0b312dd817efb7c3839549fc5562548e4eded7eb
RBI4 <rebecca.inge@gmail.com>
2017-11-16 16:25:15 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -8,4 +8,6 @@ IcebreakerResponse.BatchCreate = function(emails){
   })
 }
 
-const emails = ["avi@flatironschool.com", "grace@hopper.com", "alan@xparc.com"]
+const emails = ["avi@flatironschool.com"],
+              ["grace@hopper.com"],
+              ["alan@xparc.com"]
