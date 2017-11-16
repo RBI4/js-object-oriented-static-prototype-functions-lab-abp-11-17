312b2b58a7afa4cc066b0bd6bc2c1a81e4b96593
RBI4 <rebecca.inge@gmail.com>
2017-11-16 16:33:12 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,7 +2,7 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
-IcebreakerResponse.BatchCreate = function(email){
+IcebreakerResponse.BatchCreate = function(emails){
   return emails.map(function(email){
     //* new array based on this function
     return new IcebreakerResponse(email)
